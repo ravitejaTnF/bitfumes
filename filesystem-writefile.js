73b46799ae0fe9ba3965861a7b48e7d92e8b8d69
raviteja-informa <103987651+raviteja-informa@users.noreply.github.com>
2022-11-16 18:51:23 +0530

    commit/files
    added some more things for writing files

--- filesystem-writefile.js
+++ filesystem-writefile.js
@@ -11,11 +11,32 @@ const server = http.createServer((req,res) => {
         });
     }else if(req.url == '/create-file'){
         res.writeHead(200, { 'Content-Type': 'text/html'});
-        fs.writeFile('temp/file1.html','this is a new file',(err) => {
+        fs.appendFile('temp/file1.html','this is a new file',(err) => {
             if(err){
                 throw err;
             }
             res.write('File has been created successfully, check in the destination folder');
+            res.end();
+        })
+    }
+    else if(req.url == '/forloop'){
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        for (let index = 0; index < 10000; index++) {
+            fs.appendFile('temp/forloop.html','<h1>For loop data</h1>\n',(err) => {
+                if(err){
+                    throw err;
+                }
+            })
+        }
+        res.write('success');
+        res.end();
+    }else if(req.url == '/for'){
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        fs.readFile('temp/forloop.html',(err,data) => {
+            if(err){
+                throw err;
+            }
+            res.write(data);
             res.end();
         })
     }
