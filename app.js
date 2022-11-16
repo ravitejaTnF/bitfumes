873a04ba1e46cf398965b27f573d4370d4c97531
raviteja-informa <103987651+raviteja-informa@users.noreply.github.com>
2022-11-16 16:51:52 +0530

    commit-3
    routing in nodejs

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-// create server
-const http = require('http');
-const Circle = require('./circle');
-const PORT = 3000;
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-        JSON.stringify(
-            { data: "Hello From Server" }
-    ));
-})
+/*
+* using circle class by importing circle class
+* basic creation of server
+*/
+// const http = require('http');
+// const Circle = require('./circle');
+// const PORT = 3000;
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(
+//         JSON.stringify(
+//             { data: "Hello From Server" }
+//     ));
+// })
 
-server.listen(PORT);
+// server.listen(PORT);
 
 /*
 * using circle class by importing circle class
@@ -18,3 +21,41 @@ server.listen(PORT);
 // const circle = new Circle();
 // console.log(circle.area(5));
 // console.log(circle.perimeter(10));
+
+/*
+* creation of server then checking the route value and displaying related data according to 
+* routes
+*/
+const PORT = 3000;
+const http = require('http');
+const server1 = http.createServer((req,res) => {
+    let url = req.url;
+    if(url == '/'){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(
+            {data:"This is home page"}
+        ))
+        res.end();
+    }
+    else if(url == '/login'){
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.write(JSON.stringify(
+            {data:"This is login page"}
+        ))
+        res.end();
+    }
+    else if(url == '/signup'){
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.write(JSON.stringify(
+            {data:"This is signup page"}
+        ))
+        res.end();
+    }
+    else{
+        res.writeHead(404,{"Content-Type":"text/html"});
+        res.write('<h1>PAGE NOT FOUND</h1>');
+        res.end();
+    }
+})
+
+server1.listen(PORT);
