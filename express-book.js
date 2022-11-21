237fcbb7ec221df4a433c4b566b349b865f3281c
raviteja-informa <103987651+raviteja-informa@users.noreply.github.com>
2022-11-21 15:01:14 +0530

    commit/route-chaining
    - route chaining in express

--- express-book.js
+++ express-book.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const app = express();
 const PORT = 4444;
-app.get('/books',(req,res) => {
-    res.json({data:"All books"});
+
+/*
+* Chaining routes in Express L6-L14
+*/
+app.route('/book')
+    .get((req, res) => {
+        res.json({ data: 'Book is fetched' });
+    })
+    .post((req, res) => {
+        res.json({ data: 'Book is stored' });
+    })
+
+app.get('/book', (req, res) => {
+    res.json({ data: "All books" });
 })
 
-app.get('/books/:id',(req,res) => {
+app.post('/book', (req, res) => {
+    res.send('Book is stored..!');
+})
+
+app.get('/book/:id', (req, res) => {
     let id = req.params.id;
-    res.json({data:`Book ${id}`});
+    res.json({ data: `Book ${id}` });
 })
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 })
 
