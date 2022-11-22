f620837a825e2b05446deb627c23e08d25cfdaae
raviteja-informa <103987651+raviteja-informa@users.noreply.github.com>
2022-11-22 10:51:34 +0530

    commit/routing
    - serving html files in express

--- book/book-route.js
+++ book/book-route.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const bookRouter = require('./book-sub');
-
+const path = require('path');;
 router.get('/',(req,res) => {
-    res.send('Hello world from express server..!');
+    //res.send('Hello world from express server..!');
+    res.sendFile(path.join(__dirname,'/../pages/home.html'));
+    //console.log(__dirname);
 })
 
 router.use(bookRouter);
 
 router.get('/*',(req,res) => {
-    res.send('Page no found..😒');
+    //res.send('Page no found..😒');
+    res.sendFile(path.join(__dirname,'/../pages/page-not-found.html'));
 })
 
 module.exports = router;
